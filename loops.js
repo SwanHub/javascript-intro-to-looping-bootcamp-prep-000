ebbe511a3ff83dd3011ae8ecd843f50664f7a7e7
Jackson Prince <jdprince555@gmail.com>
2019-03-10 21:40:04 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,27 @@
+var emptySet = [];
+
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+        if (i === 1) {
+            array.push(`I am ${i} strange loop.`); }
+        else { 
+            array.push(`I am ${i} strange loops.`); }
+    }
+    return array;
+}
+
+forLoop(emptySet);
+
+let num = 20;
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(--num);
+  }
+  return "done";
+}
+
+whileLoop(num);
+
+
+
