4b780479663ffcef2e283ef1fbb1dab9a645af25
Jackson Prince <jdprince555@gmail.com>
2019-03-10 22:24:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -34,7 +34,13 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do { 
     console.log("I run once regardless."); 
-  } while (incrementVariable() < num);
+  } while (incrementVariable() < num );
+}
+
+function doWhileLoop(num) {
+  do { 
+    console.log("I run once regardless."); 
+  } while (incrementVariable() < num + 1);
 }
 
 
